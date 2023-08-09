37980fc705e8533fda86dd23162c322de31f32e9
Christoffer Hald Christensen <chrishald@outlook.dk>
2023-08-09 22:42:10 +0200

    Setting up solution with test library and executable

---
+++ The-Gumbler/Test Game/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+
+#include "testLib.h"
+
+int main()
+{
+	PrintLibFunc("This was printed from a library");
+	std::cin.get();
+}
---
+++ The-Gumbler/The-Gumbler/src/testLip.cpp
@@ -0,0 +1,8 @@
+#include "testLib.h"
+#include <iostream>
+
+
+void PrintLibFunc(const char* string)
+{
+	std::cout << string << std::endl;
+}
